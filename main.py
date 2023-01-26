0ec855455ad66ff92e5740bfe0b2575d360a2615
victor.kapral <kapviktor@gmail.com>
2023-01-26 23:59:56 +0300

    Add annotations and docstrings

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack.
+
+    Key arguments:
+    char_name -- character's name (default '')
+    char_class -- choosen class for a character (default '')
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 '{5 + randint(3, 5)}')
@@ -23,6 +29,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence.
+
+    Key arguments:
+    char_name -- character's name (default '')
+    char_class -- choosen class for a character (default '')
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -33,6 +45,12 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Abilities that are special.
+
+    Key arguments:
+    char_name -- character's name (default '')
+    char_class -- choosen class for a character (default '')
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 '{80 + 25}»')
@@ -45,6 +63,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Training.
+
+    Key arguments:
+    char_name -- character's name (default '')
+    char_class -- choosen class for a character (default '')
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -70,6 +94,10 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choose of a character class.
+
+    No key arguments.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
